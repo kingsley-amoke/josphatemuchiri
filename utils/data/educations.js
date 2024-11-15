f5a39cc1a5a1a8466d2b3dfad8468a2792eb4ad3
Kingsley Amoke <87276473+kingsley-amoke@users.noreply.github.com>
2024-11-15 11:28:43 +0100

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,24 +1,30 @@
 export const educations = [
   {
-    id: 1,
+  id: 1,
+  title:"CCNA, CCNP, CCIE, CISA",
+  duration: "2023 - 2024",
+  institution: "CISCO NETWORK WALKS ACADEMY"
+  },
+  {
+    id: 2,
     title: "Certificate in Computer Graphics and Web Design",
     duration: "2022 - 2024",
     institution: "Muyex Creativity Academy",
   },
   {
-    id: 2,
+    id: 3,
     title: "Higher Diploma in Information Technology",
     duration: "2022 - 2024",
     institution: "Friends College Kaimosi",
   },
   {
-    id: 3,
+    id: 4,
     title: "Kenya Certificate of Secondary Education(KCSE)",
     duration: "2017 - 2020",
     institution: "ST. Thomas Misikhu R.C High School",
   },
   {
-    id: 4,
+    id: 5,
     title: "Kenya Certificate of Primary Education(KCPE)",
     duration: "2006 - 2015",
     institution: "Victory Academy Primary School, Kakamega",
